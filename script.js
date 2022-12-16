09b588dfe38fddebdd61d7eda94e54bfec73bfb2
AvivaRubenstein <avivadrubenstein@gmail.com>
2022-12-16 12:28:24 -0500

    set up 5 day forecast to appear in boxes on page

--- script.js
+++ script.js
@@ -3,26 +3,27 @@ var longitude;
 var todayBox = $('#todays-forecast');
 var cityNameTodayBox = $("#cityName");
 var searchBox = $("search-box");
-var day1box = $(".5dayBox")[0];
-var cityName; 
+var dayBoxes = document.querySelectorAll(".dayBoxes");
+var cityName;
 var existingStorage;
 
 
 //TODO:Use coordinates to get weather data for that location
 //TODO: fetch request to get weather data using coordinates
-    //TODAY: city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-    //5-day forecast: date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+//TODAY: city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+//5-day forecast: date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 //TODO: get searchHistory to display on HTML page
 //TODO: get weather data  for 5 day forecast to display in .5dayBox[0]-[4]
 //TODO: figure out how to make past search history clickable to search for that term again
 
 
 //this event listener looks for when the search button is pressed, and retrieves that search term that was input by the user
-$('.btn').on("click", function (event) { 
+$('.btn').on("click", function (event) {
     event.preventDefault();
     cityName = $("#searchInput").val();
-//Once we have the city name, we can get the coordinates from the openweathermap API
+    //Once we have the city name, we can get the coordinates from the openweathermap API
     getCoordinates();
+    //with every search, we update the search history in local storage and update the display on our page
     setSearchHistory();
 });
 
@@ -49,8 +50,9 @@ var getForecast = function () {
             return response.json();
         })
         .then(function (data) {
-            console.log(data.list[0]);
+            console.log(data.list);
             setTodayBox(data.list[0]);
+            set5dayBoxes(data.list);
         })
 };
 
@@ -59,53 +61,64 @@ var getForecast = function () {
 
 
 var setSearchHistory = function () {
- //here we set existing storage equal to anything that is contained in local storage under the key "searchStorage"
- if (existingStorage === null){
- localStorage.setItem("searchStorage", JSON.stringify([cityName]));
- } else {existingStorage.push(cityName);
- localStorage.setItem("searchStorage", JSON.stringify(existingStorage)); }
+    //if nothing is in existing storage, we put the current cityName into local storage, and if there is already stuff in local storage
+    //we push the current cityName into the existing storage array, and then set the local storage to be the entire existingStorage
+    if (existingStorage === null) {
+        localStorage.setItem("searchStorage", JSON.stringify([cityName]));
+    } else {
+        existingStorage.push(cityName);
+        localStorage.setItem("searchStorage", JSON.stringify(existingStorage));
+    }
 
- }
- var loadPrevSearchHist = function () {
+}
+var loadPrevSearchHist = function () {
     existingStorage = JSON.parse(localStorage.getItem("searchStorage"));
-     //this for loop will loop through the search history and make each one appear on the page as a list item
- if(existingStorage !== null) {
- for (i=0; i< existingStorage.length; i++ )
- {
-    var histbtn = document.createElement("button");
-    histbtn.type = "submit";
-    histbtn.name = "search-history-btn";
-    histbtn.innerHTML = existingStorage[i];
-    histbtn.setAttribute("class", "history-btns");
-    histbtn.setAttribute("id", `hist-btn-${[i]}`);
-    $("#search-history-box").append(histbtn);
- } }
- 
- $(".history-btns").on("click", function (event) { 
-    event.preventDefault();
-    cityName = this.innerHTML;
-    console.log(cityName);
-    //console.log(event);
-  });
- }
+    //this for loop will loop through the search history and make each one appear on the page as a button
+    if (existingStorage !== null) {
+        for (i = 0; i < existingStorage.length; i++) {
+            var histbtn = document.createElement("button");
+            histbtn.type = "submit";
+            histbtn.name = "search-history-btn";
+            histbtn.innerHTML = existingStorage[i];
+            histbtn.setAttribute("class", "history-btns");
+            histbtn.setAttribute("id", `hist-btn-${[i]}`);
+            $("#search-history-box").append(histbtn);
+        }
+    }
+    //this event listener looks for when one of the search history buttons was clicked, and identifies the text of that button
+    $(".history-btns").on("click", function (event) {
+        event.preventDefault();
+        //we set the current cityName equal to whatever city name was clicked on in our search history
+        cityName = this.innerHTML;
+        //then we can do the process of getting coordinates for that city name, and getting and displaying the forecast for that location 
+        getCoordinates();
+    });
+}
 
 //this function sets the box for today's forecast to have text content based on what is returned from our getForecast() function
 //we are passing in data from that function as a parameter here --- in this case the first index of the data is passed in, so that we are accessing only information about the first day in the array (i.e. now)
 var setTodayBox = function (data) {
-document.getElementById("city-name").textContent = cityName;
-document.getElementById("today-temp").textContent = "Temperature: " + data.main.temp + " degrees";
-document.getElementById("today-humidity").textContent = "Humidity: " + data.main.humidity + "%";
-document.getElementById("today-wind-speed").textContent = "Wind Speed: " + data.wind.speed + " MPH";
-document.getElementById("today-image").setAttribute("src",  "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
- }
+    document.getElementById("today-date").textContent = data.dt_txt;
+    document.getElementById("city-name").textContent = cityName;
+    document.getElementById("today-temp").textContent = "Temperature: " + data.main.temp + " degrees";
+    document.getElementById("today-humidity").textContent = "Humidity: " + data.main.humidity + "%";
+    document.getElementById("today-wind-speed").textContent = "Wind Speed: " + data.wind.speed + " MPH";
+    document.getElementById("today-image").setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+}
 
+var arrayInList = 0;
+var set5dayBoxes = function (data) {
+    for (i = 0; i < dayBoxes.length; i++) {
+        document.getElementById(`today-date${[i]}`).textContent = data[arrayInList].dt_txt;
+        document.getElementById(`today-temp${[i]}`).textContent = "Temperature: " + data[arrayInList].main.temp + " degrees";
+        document.getElementById(`today-humidity${[i]}`).textContent = "Humidity: " + data[arrayInList].main.humidity + "%";
+        document.getElementById(`today-wind-speed${[i]}`).textContent = "Wind Speed: " + data[arrayInList].wind.speed + " MPH";
+        document.getElementById(`today-image${[i]}`).setAttribute("src", "http://openweathermap.org/img/w/" + data[arrayInList].weather[0].icon + ".png");
 
- //coordinates api call https://api.openweathermap.org/data/2.5/weather?q=
- //forecast api call https://api.openweathermap.org/data/2.5/onecall?lat=
+        arrayInList += 8;
+        // console.log(`today-date${[i]}`);
+    }
+}
 
- //for the icon:
- //todayImg.attr("src", "http://openweathermap.org/img/w/" + picture + ".png")
-
-
- //load previous search history onto page
+//load previous search history onto page
 loadPrevSearchHist();
